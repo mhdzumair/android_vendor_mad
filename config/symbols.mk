b572f0ec45b0509c8ce7e2dc697f713705fcc08b
mhdzumair <mhdzumair@gmail.com>
2020-04-23 10:44:45 +0530

    libmtkshim:Added vtservice & program_binary_builder symbol

--- config/symbols.mk
+++ config/symbols.mk
@@ -3,14 +3,20 @@ ifeq ($(TARGET_INCLUDE_XLOG_SYMBOLS),true)
 LINKER_FORCED_SHIM_LIBS := /system/lib/liblog.so|libmtkshim_log.so:/system/lib64/liblog.so|libmtkshim_log.so
 endif
 ifeq ($(TARGET_INCLUDE_AUDIO_SYMBOLS),true)
-LINKER_FORCED_SHIM_LIBS := $(LINKER_FORCED_SHIM_LIBS):/system/lib/hw/audio.primary.$(TARGET_BOARD_PLATFORM).so|libmtkshim_audio.so
+LINKER_FORCED_SHIM_LIBS := $(LINKER_FORCED_SHIM_LIBS):/system/vendor/lib/hw/audio.primary.$(TARGET_BOARD_PLATFORM).so|libmtkshim_audio.so
 endif
 ifeq ($(TARGET_INCLUDE_UI_SYMBOLS),true)
 LINKER_FORCED_SHIM_LIBS := $(LINKER_FORCED_SHIM_LIBS):/system/lib/libui.so|libmtkshim_ui.so:/system/lib64/libui.so|libmtkshim_ui.so
 endif
 ifeq ($(TARGET_INCLUDE_OMX_SYMBOLS),true)
-LINKER_FORCED_SHIM_LIBS := $(LINKER_FORCED_SHIM_LIBS):/system/lib/libMtkOmxVdec.so|libmtkshim_omx.so
+LINKER_FORCED_SHIM_LIBS := $(LINKER_FORCED_SHIM_LIBS):/system/vendor/lib/libMtkOmxVdec.so|libmtkshim_omx.so
 endif
 ifeq ($(TARGET_INCLUDE_GPS_SYMBOLS),true)
 LINKER_FORCED_SHIM_LIBS := $(LINKER_FORCED_SHIM_LIBS):/system/bin/mtk_agpsd|libmtkshim_gps.so
+endif
+ifeq ($(TARGET_INCLUDE_PROGRAM_BINARY_BUILDER_SYMBOLS),true)
+LINKER_FORCED_SHIM_LIBS := $(LINKER_FORCED_SHIM_LIBS):/system/vendor/bin/program_binary_builder|libmtkshim_program_binary_builder.so
+endif
+ifeq ($(TARGET_INCLUDE_VTSERVICE_SYMBOLS),true)
+LINKER_FORCED_SHIM_LIBS := $(LINKER_FORCED_SHIM_LIBS):/system/vendor/lib/libvtmal.so|libmtkshim_vtservice.so
 endif

147f6cb619080dbfac1d7040b0e7c707d78319f7
mhdzumair <mhdzumair@gmail.com>
2020-04-26 23:29:11 +0530

    libmtkshims:Added missing symbols & linking to correct files

--- libmtkshims/camera.cpp
+++ libmtkshims/camera.cpp
@@ -1,3 +1,5 @@
+#include <string>
+#include <cutils/native_handle.h>
 extern "C" {
     int _ZN12DpBlitStream10invalidateEP7timeval();
     int _ZN12DpBlitStream10invalidateEv() {
@@ -13,4 +15,11 @@ extern "C" {
     int _ZN11DpIspStream15dequeueFrameEndEv() {
         return _ZN11DpIspStream15dequeueFrameEndEPj();
     }
+    void _ZNK7android16SensorEventQueue12setEventRateEPKNS_6SensorEx() {}
+}
+extern "C" void _ZN7android13GraphicBufferC1EjjijjP13native_handleb(uint32_t inWidth, uint32_t inHeight, int inFormat,
+            uint32_t inLayerCount, uint32_t inUsage,
+            native_handle_t* inHandle, bool keepOwnership)
+{
+    _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(inWidth, inHeight, inFormat, inLayerCount, inUsage, 0, inHandle, keepOwnership);
 }

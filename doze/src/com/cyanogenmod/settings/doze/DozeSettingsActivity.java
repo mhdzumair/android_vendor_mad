24869c8ef0405af8f70ec76cf7d076271d53d36d
mhdzumair <mhdzumair@gmail.com>
2020-09-01 22:29:35 +0000

    doze:Add support for darklos theme
    Signed-off-by: mhdzumair <mhdzumair@gmail.com>

--- doze/src/com/cyanogenmod/settings/doze/DozeSettingsActivity.java
+++ doze/src/com/cyanogenmod/settings/doze/DozeSettingsActivity.java
@@ -17,17 +17,37 @@
 
 package com.cyanogenmod.settings.doze;
 
+import android.app.IThemeCallback;
+import android.app.ThemeManager;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.MenuItem;
+import android.provider.Settings.Secure;
 
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 
 public class DozeSettingsActivity extends SettingsDrawerActivity {
 
     private static final String TAG_DOZE = "doze";
+    private int mTheme;
+    private ThemeManager mThemeManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final int themeMode = Secure.getInt(getContentResolver(),
+                Secure.THEME_PRIMARY_COLOR, 1);
+        final int accentColor = Secure.getInt(getContentResolver(),
+                Secure.THEME_ACCENT_COLOR, 0);
+        mThemeManager = (ThemeManager) getSystemService(Context.THEME_SERVICE);
+        if (mThemeManager != null) {
+            mThemeManager.addCallback(mThemeCallback);
+        }
+        if (themeMode != 0 || accentColor != 0) {
+            getTheme().applyStyle(mTheme, true);
+        }
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.doze);
@@ -35,6 +55,22 @@ public class DozeSettingsActivity extends SettingsDrawerActivity {
         getFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new DozeSettingsFragment(), TAG_DOZE).commit();
     }
+
+    private final IThemeCallback mThemeCallback = new IThemeCallback.Stub() {
+
+       @Override
+       public void onThemeChanged(int themeMode, int color) {
+           onCallbackAdded(themeMode, color);
+           DozeSettingsActivity.this.runOnUiThread(() -> {
+               DozeSettingsActivity.this.recreate();
+           });
+       }
+
+       @Override
+       public void onCallbackAdded(int themeMode, int color) {
+           mTheme = color;
+       }
+   };
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
